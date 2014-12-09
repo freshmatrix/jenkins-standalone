7ad5dd3048f55960866477dc279dd836be685024
Roger Ignazio <roger@puppetlabs.com>
2014-12-08 21:11:16 -0800

    EXPERIMENTAL: master-only jenkins deployments
    This experimental commit allows for master-only Jenkins deployments.
    Instead of having Jenkins masters (running on Mesos) provision slaves
    on-demand (also running on Mesos), it shifts the ownership of the
    executors to the masters. This allows for easier global queueing (based
    on threads instead of resources) and only requires scaling the masters.
    
    It's not yet known what the implications of this would be, but it might
    be worth exploring. Or this branch might be a waste of time. We'll find
    out.

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # jenkins-standalone
 Everything needed to run a Jenkins master on Apache Mesos and Marathon.
 
-## Usage
-`jenkins-standalone.sh` takes two arguments:
-  - ZooKeeper URL
-  - Redis host
+This repo assumes that you want to run a cluster of Jenkins masters, with the
+masters also holding the executors, so that you can horizontally scale your
+CI system. It does not provision Mesos slaves (although it can).
 
+## Usage
+`jenkins-standalone.sh` takes one argument: the host Redis is running on.
 Redis is used as the broker for Logstash and the Jenkins Logstash plugin.
 
 When copying/pasting this command into Marathon, each line should be
@@ -16,5 +17,5 @@ Example usage:
 ```
 git clone https://github.com/rji/jenkins-standalone
 cd jenkins-standalone
-./jenkins-standalone.sh -z zk://10.132.188.212:2181[, ... ]/mesos -r redis.example.com
+./jenkins-standalone.sh -r redis.example.com
 ```
--- jenkins-standalone.sh
+++ jenkins-standalone.sh
@@ -36,9 +36,9 @@ if [[ $? != 0 ]]; then
     exit 1
 fi
 
-# Accept ZooKeeper paths on the command line
-if [[ ! $# > 3 ]]; then
-    echo "Usage: $0 -z zk://10.132.188.212:2181[, ... ]/mesos -r redis.example.com"
+# Accept Redis paths on the command line
+if [[ ! $# > 1 ]]; then
+    echo "Usage: $0 -r redis.example.com"
     echo
     exit 1
 fi
@@ -47,10 +47,6 @@ while [[ $# > 1 ]]; do
     key="$1"
     shift
     case $key in
-        -z|--zookeeper)
-            ZOOKEEPER_PATHS="$1"
-            shift
-            ;;
         -r|--redis-host)
             REDIS_HOST="$1"
             shift
@@ -76,7 +72,6 @@ for plugin in ${JENKINS_PLUGINS[@]}; do
 done
 
 # Jenkins config files
-sed -i "s!_MAGIC_ZOOKEEPER_PATHS!${ZOOKEEPER_PATHS}!" config.xml
 sed -i "s!_MAGIC_REDIS_HOST!${REDIS_HOST}!" jenkins.plugins.logstash.LogstashInstallation.xml
 sed -i "s!_MAGIC_JENKINS_URL!http://${HOST}:${PORT}!" jenkins.model.JenkinsLocationConfiguration.xml
 
